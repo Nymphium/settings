193df368812542978f806eb2e6d3e112e3a1e8f0
Nymphium <s1311350@coins.tsukuba.ac.jp>
2014-03-25 08:48:07 +0900

    20140325_08:48:07

--- settingfies/.tmux/getVolume.sh
+++ settingfies/.tmux/getVolume.sh
@@ -1,3 +1,5 @@
+# AMIX=`amixer -c 0 set Master 0%- | awk 'NR==5'
+
 MUTE=`amixer -c 0 set Master 0%- | grep -G on.$ | awk 'END { if(NR > 1){print ""} else {print "#[bg=red]"} }'`
 
 VOL=`amixer -c 0 set Master 0%- | awk NR==5 | sed -e "s/.*\[\([0-9]*\)%\].*/\1/"`
--- settingfies/.tmux/rightside.sh
+++ settingfies/.tmux/rightside.sh
@@ -1,1 +1,4 @@
+# before you have to install "tmux-mem-cpu-load"
+# >> https://github.com/thewtex/tmux-mem-cpu-load
+
 tmux-mem-cpu-load 2 | sed -e "s/\(.*MB\).*]\ *\([0-9]*\.[0-9]%\) .*/Mem:\1 CPU:\2/"
