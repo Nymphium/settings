c2d15e05dbc2924898bebc4fcf13082c094bc5a9
Nymphium <s1311350@coins.tsukuba.ac.jp>
2014-05-28 05:17:42 +0900

    20140528_05:17:42

--- settingfies/.tmux/rightside.sh
+++ settingfies/.tmux/rightside.sh
@@ -1,5 +1,7 @@
 # memory
-free -m | sort -n | awk 'NR==1{printf "Mem:"$3"/"}NR==2{printf $2"MB => "}'; ps aux | awk '{printf $4 " " $11 "\n"}' | sort -r | awk 'NR==2{printf $2 "(" $1 "%)"}'
+free -m | sort -n | awk 'NR==1{printf "Mem:"$3"/"}NR==2{printf $2"MB"}'; ps aux | sed -e "s/\/[^ ]*\///g" | awk '{printf $4 " " $11 "\n"}' | sort -r | awk 'NR==2{printf " Max:" $2 "(" $1 "%)"}'
+
+printf "|"
 
 # cpu
-vmstat -w 1 2 | awk 'NR==4{printf " CPU:%d%", (100 - $15)}'
+vmstat -w 1 2 | awk 'NR==4{printf "CPU:%d% Max:", (100 - $15)}'; ps aux | sed -e "s/\/[^ ]*\///g" | awk '{printf $3 " " $11 "\n"}' | sort -r | awk 'NR==2{printf " Max:" $2 "(" $1 "%)"}'
