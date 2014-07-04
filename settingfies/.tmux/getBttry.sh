2813a9dc2f6c18f8e4b5be5be4cda2d0490bb548
Nymphium <s1311350@coins.tsukuba.ac.jp>
2014-07-04 14:46:56 +0900

    20140704_14:46:56

--- settingfies/.tmux/getBttry.sh
+++ settingfies/.tmux/getBttry.sh
@@ -1,11 +1,27 @@
 ACPIB=`acpi -b`
 
 if [ ${#ACPIB} -gt 1 ]; then
-	COLOR=`acpi -b | sed -e "s/.* \([0-9]*\)%.*/\1/g" | awk '{if($1 <= 10){print "#[fg=yellow,bg=colour196]"}else if($1 <= 20){print "#[fg=black,bg=yellow]"}else{print "#[fg=colour76,bg=colour235]"}}'`
+	COLOR=`acpi -b | sed -e "s/.* \([0-9]*\)%.*/\1/g" | \
+		awk '{
+			if($1 <= 10){
+				print "#[fg=yellow,bg=colour196]"
+			}else if($1 <= 20){
+				print "#[fg=black,bg=yellow]"
+			}else{
+				print "#[fg=colour76,bg=colour235]"
+			}
+		}'`
 
-	# COLOR="#[fg=yellow,bg=colour196]"
+	BATTERY=`acpi -b | \
+		awk '{
+			if($3=="Charging," || $3=="Unknown,"){
+				print "Charging",$4
+			}else{
+				print $4,$5
+			}
+		}' | \
 
-	BATTERY=`acpi -b | awk '{if($3=="Charging," || $3=="Unknown,"){print "Charging",$4}else{print $4,$5}}' | sed -e "s/,\|\s$//g" -e "s/%/%%/"`
+		sed -e "s/,\|\s$//g" -e "s/%/%%/"`
 
 	printf "${COLOR}Bat:${BATTERY}＞"
 fi
--- settingfies/.tmux/getVolume.sh
+++ settingfies/.tmux/getVolume.sh
@@ -1,5 +1,12 @@
 if [ `amixer | awk '{printf $1}'` != "Failed" ]; then
-	MUTE=`amixer -c 0 set Master 0%- | grep -G on.$ | awk 'END { if(NR > 1){print "#[fg=colour255]"} else {print "#[fg=colour196]"} }'`
+	MUTE=`amixer -c 0 set Master 0%- | grep -G on.$ | \
+		awk 'END {
+			if(NR > 1){
+				print "#[fg=colour255]"
+			}else{
+				print "#[fg=colour196]"
+			}
+		}'`
 
 	VOL=`amixer -c 0 set Master 0%- | awk NR==5 | sed -e "s/.*\[\([0-9]*\)%\].*/\1/"`
 
