710de64b20d1f851f0701b49da502ebdfb8e678e
Nymphium <s1311350@coins.tsukuba.ac.jp>
2014-07-30 14:36:44 +0900

    20140730_14:36:44

---
+++ bin/setwondowheight.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+ARRAY=(`xdotool getactivewindow getwindowgeometry | awk 'NR==3{print $2}' | sed -e "s/x/ /"`)
+
+# SIZE=""
+
+# SET_WINDOWSIZE="xdotool getactivewindow windowsize ${SIZE}"
+
+while test $# -gt 0; do
+	case "$1" in
+		-l|--right)
+			SIZE="`expr ${ARRAY[0]} + 80` ${ARRAY[1]}"
+
+			xdotool getactivewindow windowsize ${SIZE}
+
+			exit 0
+
+			;;
+		-h|--left)
+			SIZE="`expr ${ARRAY[0]} - 80` ${ARRAY[1]}"
+
+			xdotool getactivewindow windowsize ${SIZE}
+
+			exit 0
+
+			;;
+		-k|--up)
+			SIZE="${ARRAY[0]} `expr ${ARRAY[1]} + 30`"
+
+			xdotool getactivewindow windowsize ${SIZE}
+
+			exit 0
+
+			;;
+		-j|--down)
+			SIZE="${ARRAY[0]} `expr ${ARRAY[1]} - 30`"
+
+			xdotool getactivewindow windowsize ${SIZE}
+
+			exit 0
+
+			;;
+		*)
+			exit 1
+			;;
+	esac
+done
