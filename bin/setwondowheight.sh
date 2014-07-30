58249a9d19d81afd55bc15f9a878f4b8890541ad
Nymphium <s1311350@coins.tsukuba.ac.jp>
2014-07-30 14:45:42 +0900

    20140730_14:45:42

--- bin/setwondowheight.sh
+++ bin/setwondowheight.sh
@@ -3,7 +3,7 @@
 ARRAY=(`xdotool getactivewindow getwindowgeometry | awk 'NR==3{print $2}' | sed -e "s/x/ /"`)
 
 function _setwindowsize(){
-	xdotool getactivewindow windowsize $1
+	xdotool getactivewindow windowsize $@
 }
 
 while test $# -gt 0; do
@@ -11,37 +11,27 @@ while test $# -gt 0; do
 		-l|--right)
 			SIZE="`expr ${ARRAY[0]} + 80` ${ARRAY[1]}"
 
-			_setwindowsize ${SIZE}
-
-			exit 0
-
 			;;
 		-h|--left)
 			SIZE="`expr ${ARRAY[0]} - 80` ${ARRAY[1]}"
-
-			_setwindowsize ${SIZE}
-
-			exit 0
 
 			;;
 		-k|--up)
 			SIZE="${ARRAY[0]} `expr ${ARRAY[1]} + 30`"
 
-			_setwindowsize ${SIZE}
-
-			exit 0
-
 			;;
 		-j|--down)
 			SIZE="${ARRAY[0]} `expr ${ARRAY[1]} - 30`"
-
-			_setwindowsize ${SIZE}
-
-			exit 0
 
 			;;
 		*)
 			exit 1
 			;;
 	esac
+
+	echo ${SIZE}
+
+	_setwindowsize ${SIZE}
+
+	exit 0
 done
