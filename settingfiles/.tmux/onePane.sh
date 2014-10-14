042a099550280246c0e71bd2f92fe258a1fb9b26
Nymphium <s1311350@coins.tsukuba.ac.jp>
2014-10-15 01:42:07 +0900

    20141015_01:42:07

--- settingfiles/.tmux/onePane.sh
+++ settingfiles/.tmux/onePane.sh
@@ -4,9 +4,8 @@ WIN_NUM=`tmux display -p "#{session_windows}"`
 
 if [ ${PANE_NUM} -eq 1 -a ${WIN_NUM} -eq 1 ]; then
 	tmux split-window -c $HOME
+
 	tmux 'kill-pane' -t 0
 else
 	tmux 'kill-pane'
-	# echo ${PANE_NUM}
-	# echo ${WIN_NUM}
 fi
