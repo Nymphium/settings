e9cb738405380daaa44053f39f3695700228812e
Nymphium <s1311350@coins.tsukuba.ac.jp>
2014-12-15 00:38:19 +0900

    20141215_00:38:19

--- settingfiles/.tmux/statuslines/getMemCPU.sh
+++ settingfiles/.tmux/statuslines/getMemCPU.sh
@@ -1,22 +1,18 @@
 free=`which free`
 if [ ${free} ]; then
 	# memory
-	${free} -m | awk 'NR==2{printf "#[bg=colour235,fg=colour76]Mem:%d/%sMB ", $3, $2}'
+	${free} -m | awk 'BEGIN{{total = 0}{used = 0}} {{total += $2}{used += $3}} END{printf "#[bg=colour235,fg=colour76]Mem:%d/%sMB ", used, total}'
 
-	ps aux | sed -e "s/\/[^ ]*\///g" | awk '{printf $4 " " $11 "\n"}' | sort -r | awk 'NR==2{printf "#[fg=colour6]Max:%s(%2.1f%%)＞", $2, $1}'
+	ps -axho "%c%C" --sort -%cpu | awk 'NR==1{printf "#[fg=colour6]Max:%s(%2.1f%%)＞", $1, $2}'
 fi
 
 vmstat=`which vmstat`
 
 if [ ${vmstat} ]; then
 	# cpu
-	${vmstat} -w 1 2 | awk 'NR==4{printf "#[fg=colour76]CPU:%d% ", (100 - $15)}'
+	vmstat -w 1 2 | awk 'NR==4{printf "#[fg=colour76]CPU:%d% ", (100 - $15)}'
 
-	cpu_procnum=`awk 'BEGIN{i = 0} $1 == "physical"'{i++}'END{print i}' /proc/cpuinfo`
-
-	line_num=`expr 7 + ${cpu_procnum}`
-
-	top -bn1 -o %CPU | awk NR==${line_num} | awk '{printf "#[fg=colour226]Max:%s(%2.1f%)＞", $11, $7}'
+	top -bn1 -o %CPU | awk 'BEGIN{d1 = ""}{if(d1 == "PID"){exit} d1 = $1} END{printf "#[fg=colour226]Max:%s(%2.1f%%)＞", $11, $7}'
 fi
 
 echo "#[fg=colour235]"
