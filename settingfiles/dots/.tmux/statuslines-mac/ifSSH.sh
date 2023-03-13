24b55cd6a5242aba5a5a6d7beff4d5e2356b14bd
Nymphium <s1311350@gmail.com>
2023-03-13 18:04:17 +0900

    update

--- settingfiles/dots/.tmux/statuslines-mac/ifSSH.sh
+++ settingfiles/dots/.tmux/statuslines-mac/ifSSH.sh
@@ -1,4 +1,4 @@
-HAS_SSH=$(tmux showenv SSH_CONNECTION 2> /dev/null | sed -e 's/SSH_CONNECTION=\(\S\+\).*$/\1/')
+HAS_SSH=$(tmux showenv SSH_CONNECTION | awk '{ gsub(/.*=/, "", $1); print $1}')
 
 if [[ "${#HAS_SSH}" -gt 2 ]] && [[ ! "${SSHARG}" =~ ^-.* ]]; then
 	echo "[#[fg=colour47,bold]SSH from ${HAS_SSH}#[fg=colour27,bold]] "
